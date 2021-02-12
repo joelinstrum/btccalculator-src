b6ed40c8b1f5fd050a1be8cb2baf7de005303125
Joe Linstrum <jlinstrum@altitudenetworks.com>
2021-02-11 17:50:13 -0800

    added blurb

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -17,6 +17,7 @@ const Home = () => {
 
   return (<div>
             <div className="title-large">Crypto ROI Calculator</div>
+            
             <CryptoCalculator />
             {
               childCalculators.map((item, n) => <CryptoCalculator 
@@ -30,6 +31,15 @@ const Home = () => {
             <button onClick={ () => {
               setChildCalculators(childCalculators => [...childCalculators, {id:childCalculators.length }])
             }}>Compare +</button>
+            </div>
+            <div className="blurb">
+              I needed a quick way to determine future crypto profit projections with my investment. For example,
+              if I invested $25,000 in Ethereum at a current price of $1700, what would my return look like
+              if the future price per coin rose to $1800? What if it rose to $3000? <br />
+              How would that compare to an investment in Bitcoin? <br />
+              Use the bitcoin / crypto future calculator above. <br /><br />
+              Also, I'm open to suggestions!<br />
+              <a href="https://twitter.com/JoeL42737181" className="link-light">Find me on Twitter :-)</a><br />
             </div>
           </div>)
 }
