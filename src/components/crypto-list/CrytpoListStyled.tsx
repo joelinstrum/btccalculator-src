c1dccbdc413c786b65b1231a4ed735c9391ff41e
Joe Linstrum <joelinstrum@gmail.com>
2023-03-08 17:13:18 -0800

    Added some spacing on the crypotlist modal

--- src/components/crypto-list/CrytpoListStyled.tsx
+++ src/components/crypto-list/CrytpoListStyled.tsx
@@ -4,6 +4,9 @@ const CryptoListStyled = styled("div")(({ theme }) => ({
   color: theme.palette.text.modalPrimary,
   fontSize: 13,
   margin: "5px",
+  "& .MuiTypography-h4": {
+    marginBottom: "8px",
+  },
 }));
 
 export default CryptoListStyled;
